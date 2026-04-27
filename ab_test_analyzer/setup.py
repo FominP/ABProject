5dd9147f9178b3f767468815b37a874f1a6affc4
Pavel Fomin <fominpavel243@gmail.com>
2026-04-27 22:13:22 +0300

    Fix package structure: move code to nested folder, update to version 0.1.3

--- ab_test_analyzer/setup.py
+++ ab_test_analyzer/setup.py
@@ -2,25 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="ab_test_analyzer",
-    version="0.1.0",
-    author="Pavel Fomin",
-    author_email="fominpavel243@gmail.com",
-    description="Библиотека для статистического анализа A/B-тестов",
-    long_description="",
-    long_description_content_type="text/markdown",
-    url="https://github.com/FominP/ABProject",
+    version="0.1.3",
     packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.8",
     install_requires=[
         "numpy>=1.21.0",
         "scipy>=1.7.0",
     ],
-    extras_require={
-        "dev": ["pytest>=7.0", "flask>=2.0"],
-    },
+    description="Библиотека для статистического анализа A/B-тестов",
+    long_description="Библиотека для A/B-тестов: z-тест, t-тест, хи-квадрат, SRM, MDE",
+    long_description_content_type="text/markdown",
+    author="Pavel Fomin",
+    author_email="fominpavel243@gmail.com",
+    url="https://github.com/FominP/ABProject",
+    python_requires=">=3.8",
 )
